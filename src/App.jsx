82752acb64f08c201d3cd965c73dc49f65ca3eb8
hyeonji817 <guswl930817@gmail.com>
2024-10-25 18:11:09 +0900

    리싸이프로 페이지 재편성

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useReducer, useRef, createContext } from "react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import Home from "../main/Home";
 import "./App.css";
 
-console.log(import.meta.url); // 현재 파일 경로 확인
-console.log(new URL('../01_main/Home.jsx', import.meta.url)); // Home.jsx 경로 확인
-
+// 1. "/" : 계정, 상품, 게시판, 마이페이지, 고객센터 등을 조회하는 메인페이지
+// 2. "/login" : 계정을 관리하는 Login 페이지
+// 3. "/Products" : 상품을 판매 및 관리하는 상품리스트 페이지 
+// 4. "/mypage" : 개인정보를 조회하는 마이페이지 
+// 5. "/review" : 게시글 작성, 수정, 신고 등을 담당하는 게시판 페이지 
+// 6. "/custoner" : 고객센터 관리페이지 
+// 7. "/index_admin" : 관리자페이지 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-};
+  return <></>;
+}
 
 export default App;
