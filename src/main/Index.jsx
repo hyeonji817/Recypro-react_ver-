87b59240a69e6d7696435539e78cd9538a1661e5
hyeonji817 <guswl930817@gmail.com>
2024-10-26 22:48:49 +0900

    Index.jsx 페이지 코드구현 2차

--- src/main/Index.jsx
+++ src/main/Index.jsx
@@ -1,41 +1,30 @@
 import "./Index.css";
-import { Carousel } from 'react-bootstrap';
+// import { Carousel } from 'react-bootstrap';
 
 const Index = () => {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img 
-          className="d-block w-100"
-          src="./assets/main1_2.png"
-          alt="재활용품 쇼핑몰"
-        />
-        <Carousel.Caption>
-          <p>재활용품 쇼핑몰</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img 
-          className="d-block w-100"
-          src="./assets/main2_2.png"
-          alt="지구지킴의 선두주자!"
-        />
-        <Carousel.Caption>
-          <p>지구지킴의 선두주자!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img 
-          className="d-block w-100"
-          src="./assets/cherryblossom_2.png"
-          alt="재활용 상품으로 환경을 보존하자"
-        />
-        <Carousel.Caption>
-          <p>재활용 상품으로 </p>
-          <p>환경을 보존하자</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="wrap_center" id="wrap-center">
+      <div className="banner-container" id="banner-container">
+        <div className="img_wrap">
+          <ul className="rolling_imgs">
+            <li>
+              <img src="./assets/main1_2.png"/>
+              <p className="text">재활용품 쇼핑몰</p>
+            </li>
+            <li>
+              <img src="./assets/main2_2.png"/>
+              <p className="text">지구지킴의 선두주자!</p>
+            </li>
+            <li>
+              <img src="./assets/cherryblossom_2.png"/>
+              <p className="text">
+                재활용 상품으로 <br /> 환경을 보존하자
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
