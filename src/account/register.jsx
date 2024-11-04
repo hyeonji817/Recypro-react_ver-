7248daa74c9a6a60f9e2d1d82b9156f2e40f7d69
hyeonji817 <guswl930817@gmail.com>
2024-11-04 22:02:32 +0900

    회원가입 페이지 UI 2차 구현 (입력란 간격 조절 및 성별 배치 조절)

--- src/account/register.jsx
+++ src/account/register.jsx
@@ -10,38 +10,53 @@ const register = () => {
         </h1>
         <form>
           <h1 className="h3 mb-3 font-weight-normal" id="register">회원가입</h1>
-          <div>
+          <div className="form-group">
             <label>ID</label>
-            <input /><br />
-
-            <label>password</label>
-            <input /><br />
-
-            <label>이름</label>
-            <input /><br />
-
-            <label>나이</label>
-            <input /><br />
-
-            <label>성별</label><br />
-            <input type="radio" name="gender" checked />남성
-            <input type="radio" name="gender" />여성
-            <br />
-            <br />
-
-            <label>이메일</label>
-            <input type="email" name="email" id="email" placeholder="ex) abc@gmail.com" required/>
-            <br />
-
-            <Button
-              text={"회원가입"}
-              type={"register"}
-            />
-            <Button 
-              text={"뒤로가기"}
-              type={"register_back"}
-            />
+            <input type="text" className="form-control" placeholder="ID를 입력하세요"/><br />
           </div>
+          
+          <div className="form-group">
+            <label>password</label>
+            <input type="password" className="form-control" placeholder="비밀번호를 입력하세요"/><br />
+          </div>
+            
+          <div className="form-group">
+            <label>이름</label>
+            <input type="text" className="form-control" placeholder="이름을 입력하세요"/><br />
+          </div>
+
+          <div className="form-group">
+            <label>나이</label>
+            <input type="number" className="form-control" placeholder="나이를 입력하세요"/><br />
+          </div>
+
+          <div className="form-group">
+            <label className="user-only">성별</label><br />
+            <div className="gender-group">
+              <label>
+                <input type="radio" name="gender" className="male" defaultChecked />남성
+              </label>
+              <label>
+                <input type="radio" name="gender" className="female" />여성
+              </label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>이메일</label>
+            <input type="email" className="form-control" placeholder="ex) abc@gmail.com" required/>
+            <br />
+          </div>
+            
+          <Button
+            text={"회원가입"}
+            type={"register"}
+          />
+          <Button 
+            text={"뒤로가기"}
+            type={"register_back"}
+          />
+          
         </form>
       </div>
     </div>
