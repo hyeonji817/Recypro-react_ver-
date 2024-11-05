28a215070f22174f9f83555537dc14b6036b1718
hyeonji817 <guswl930817@gmail.com>
2024-11-05 21:29:55 +0900

    아이디찾기 페이지(find_id.jsx) UI 구현

--- src/account/find_id.jsx
+++ src/account/find_id.jsx
@@ -4,29 +4,31 @@ import Button from "../pages/Button";
 
 const find_id = () => {
   return (
-    <div className="card align-middle">
-      <div className="card-title">
-        <h2 className="card-title">
-          <img src={logo} />
-        </h2>
-      </div>
+    <div className="wrapper_findId">
+      <div className="card align-middle">
+        <div className="card-title">
+          <h2 className="card-title">
+            <img src={logo} />
+          </h2>
+        </div>
 
-      <div className="card-body">
-        <form className="form-signin">
-          <p className="text2">이름</p>
-          <input type="text" name="name" id="name" />
-          <input type="email" name="email" id="email" />
-          <p className="check" id="check">확인</p>
-          <Button 
-            text={"아이디찾기"}
-            type={"find_id"}
-          />
-        </form>
-      </div>
+        <div className="card-body">
+          <form className="form-signin">
+            <p className="text2"></p>
+            <input type="text" name="name" id="name" className="form-control" placeholder="이름" />
+            <input type="email" name="email" id="email" className="form-control" placeholder="이메일" />
+            <p className="check" id="check"></p>
+            <Button 
+              id="btn-Yes"
+              text={"아이디찾기"}
+              type={"find_id"}
+            />
+          </form>
+        </div>
 
-      <div className="links">
-        <a>비밀번호 찾기</a>
-        <a>로그인</a> | <a>회원가입</a>
+        <div className="links">
+          <a>비밀번호 찾기</a> | <a>로그인</a> | <a>회원가입</a>
+        </div>
       </div>
     </div>
   );
