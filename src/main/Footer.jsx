760351e4274ec6479f1957f0f54c486b184db2e5
hyeonji817 <guswl930817@gmail.com>
2024-11-09 18:17:14 +0900

    메인페이지 내 Footer 컴포넌트 UI 2차 수정

--- src/main/Footer.jsx
+++ src/main/Footer.jsx
@@ -3,9 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer_bar">
-      <div className="container">
+      <div className="footer_copyrt">
         <div className="copyrt">Copyright &copy; Your Website 2022</div>
-        <div style={{ clear: "both" }}></div>
       </div>
     </div>
   );
