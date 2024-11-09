837a0e4e21afb453082b8b6eda99d58fe1210f30
hyeonji817 <guswl930817@gmail.com>
2024-11-09 18:07:59 +0900

    메인페이지 내footerjsx 페이지 컴포넌트 UI 수정

--- src/main/Footer.jsx
+++ src/main/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer_bar">
       <div className="container">
-        <div className="m-0 text-center text-white" id="copyrt">Copyright &copy; Your Website 2022</div>
+        <div className="copyrt">Copyright &copy; Your Website 2022</div>
         <div style={{ clear: "both" }}></div>
       </div>
     </div>
--- src/main/Shop_event.jsx
+++ src/main/Shop_event.jsx
@@ -10,62 +10,64 @@ const Shop_event = () => {
   const nav = useNavigate();
 
   return (
-    <div className="coupon">
-      <ul>
-        <li>
-          <span className="gift">
-            <img src={gift} alt="선물 아이콘" />
-          </span>
-          <span className="wallet">
-            <img src={wallet} alt="지갑 아이콘" />
-          </span>
-        </li>
-        <li>
-          <div className="event_title">
-            <h2>즐거운 쇼핑 Tip ★</h2>
-          </div>
-          <div className="event_info">
-            <Button 
-              onClick={() => nav("/event")}  /** 임의로 구현 */
-              text={"더 많은 혜택 보러가기"}
-              type={"event"}
-            />
-          </div>
-        </li>
-        <li>
-          <div className="kakaotalk_plus">
-            <div className="kakaotalk_title">
-              <h5>카카오톡 플러스 친구 추가 혜택 ★</h5>
-            </div>
-            <div className="kakaotalk_icon">
-              <img src={kakaotalk} alt="카카오톡 아이콘" />
-            </div>
-            <span className="kakaotalk_info">
-              <h5>20% 할인쿠폰 증정!</h5>
+    <div className="event_wrap">
+      <div className="event">
+        <ul>
+          <li>
+            <span className="gift">
+              <img src={gift} alt="선물 아이콘" />
             </span>
-            <div className="kakaotalk_go">
-              <a href="#">
-                <h6>카카오톡 플친 바로가기</h6>
-              </a>
+            <span className="wallet">
+              <img src={wallet} alt="지갑 아이콘" />
+            </span>
+          </li>
+          <li>
+            <div className="event_title">
+              <h2>즐거운 쇼핑 Tip ★</h2>
             </div>
-          </div>
-        </li>
-        <li>
-          <div className="join_event">
-            <div className="join_event_title">
-              <h5>리싸이프로 신규가입 혜택★</h5>
+            <div className="event_info">
+              <Button 
+                onClick={() => nav("/event")}  /** 임의로 구현 */
+                text={"더 많은 혜택 보러가기"}
+                type={"event"}
+              />
             </div>
-            <div className="join_info">
-              <h4>10,000원 + 각종 할인쿠폰 증정!</h4>
+          </li>
+          <li>
+            <div className="kakaotalk_plus">
+              <div className="kakaotalk_title">
+                <h5>카카오톡 플러스 친구 추가 혜택 ★</h5>
+              </div>
+              <div className="kakaotalk_icon">
+                <img src={kakaotalk} alt="카카오톡 아이콘" />
+              </div>
+              <span className="kakaotalk_info">
+                <h5>20% 할인쿠폰 증정!</h5>
+              </span>
+              <div className="kakaotalk_go">
+                <a href="#">
+                  <h6>카카오톡 플친 바로가기</h6>
+                </a>
+              </div>
             </div>
-            <div className="join_go">
-              <a href="#">
-                <h6>회원가입 하러가기 ▷</h6>
-              </a>
+          </li>
+          <li>
+            <div className="join_event">
+              <div className="join_event_title">
+                <h5>리싸이프로 신규가입 혜택★</h5>
+              </div>
+              <div className="join_info">
+                <h4>10,000원 + 각종 할인쿠폰 증정!</h4>
+              </div>
+              <div className="join_go">
+                <a href="#">
+                  <h6>회원가입 하러가기 ▷</h6>
+                </a>
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
