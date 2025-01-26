12ca6dfb311f6ef6912d8aeabc6e1b46712f5839
hyeonji817 <guswl930817@gmail.com>
2025-01-26 23:54:54 +0900

    인기상품 페이지 경로 수정

--- client/src/main/Popular_pd.jsx
+++ client/src/main/Popular_pd.jsx
@@ -8,7 +8,7 @@ const Popular_pd = () => {
     // API에서 인기 상품 데이터 가져오기 
     const fetchProducts = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/popular_product');
+        const response = await fetch('http://localhost:5003/api/popular_product');
         const data = await response.json(); // response 변수값을 json 파일로 변환 
         console.log("Fetched Products:", data);   // 데이터 확인 
         setProducts(data); 
@@ -28,7 +28,7 @@ const Popular_pd = () => {
       <div className="product-list">
         {products.map((product) => (
           <div className="product-item" key={product.productId}>
-            <img src={`http://localhost:5000/uploads/${product.filename}`} alt={product.pname} />
+            <img src={`http://localhost:5003/uploads/${product.filename}`} alt={product.pname} />
             <h3 className="pd_name">{product.pname}</h3>
             <p className="pd_description">{product.description}</p>
             <div className="product-details">
