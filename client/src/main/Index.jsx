af761bbbcd0ede273607af0c47045db1e25e051c
hyeonji817 <guswl930817@gmail.com>
2025-02-27 00:24:06 +0900

    index.jsx 1st 배너 수정

--- client/src/main/Index.jsx
+++ client/src/main/Index.jsx
@@ -1,6 +1,5 @@
 import "./Index.css";
 import "../js/Index.js";
-// import "../js/banner.js";
 import { useEffect } from "react";
 import mainImage1 from "../assets/main1_2.png";
 import mainImage2 from "../assets/main2_2.png";
@@ -11,44 +10,23 @@ import calender from "../assets/calender.png";
 import box from "../assets/box.png"; 
 
 const Index = () => {
-  // 컴포넌트가 마운트된 후 배너 초기화 
-  useEffect(() => {
-    // DOMContentLoaded 이벤트 사용으로 초기화 보장 
-    const handleLoad = () => {
-      console.log("DOMContentLoaded 이벤트 호출됨");
-      if (window.banner) {
-        window.banner.rollInit(4000);
-      }
-    };
-  
-    window.onload = handleLoad;
-  
-    return () => {
-      window.onload = null;
-    };
-  }, []);
-
   return (
     <div className="wrap_center" id="wrap-center">
-      <div className="banner-container" id="banner-container">
-        <div className="img_wrap">
-          <ul className="rolling_imgs">
-            <li className="currentroll">
-              <img src={mainImage1} alt="배너 이미지 1" />
-              <p className="text">재활용품 쇼핑몰</p>
-            </li>
-            <li className="nextroll">
-              <img src={mainImage2} alt="배너 이미지 2" />
-              <p className="text">지구지킴의 선두주자!</p>
-            </li>
-            <li>
-              <img src={cherryBlossom} alt="배너 이미지 3" />
-              <p className="text">
-                재활용 상품으로 <br /> 환경을 보존하자
-              </p>
-            </li>
-          </ul>
+      {/** 1st 배너 */}
+      <div className="banner-container" id="banner-ct">
+        <div id="intro_index">
+          <div className="cont">
+            <div className="banner1">
+              <iframe
+                src="https://player.vimeo.com/video/346223293?autoplay=1&byline=0&controls=0&loop=1&muted=1&portrait=0&sidedock=0&title=0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+                className="video-frame"
+              ></iframe>
+            </div>
+          </div>
         </div>
+        <a href="./Home.jsx" className="link_intro"></a>
       </div>
 
       {/** 2nd 배너 */}
