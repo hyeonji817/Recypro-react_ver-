48a205e630a55db96d91b85b94720ebd332e3da8
hyeonji817 <guswl930817@gmail.com>
2025-03-01 01:40:30 +0900

    상품리뷰 작성내역 페이지 코드 일부 수정

--- client/src/mypage/productReview_history.jsx
+++ client/src/mypage/productReview_history.jsx
@@ -63,21 +63,21 @@ const productReview_history = () => {
               <td>1</td>
               <td>깔끔해서 좋아요</td>
               <td>강화 소창 수건 - 먼지 안 나는 무형광</td>
-              <td>witch0817</td>
+              <td>guswl0817</td>
               <td>2024-11-23</td>
             </tr>
             <tr className="light">
               <td>2</td>
               <td>저한테 잘 맞아요</td>
               <td>유기농 설거지 비누</td>
-              <td>witch0817</td>
+              <td>guswl0817</td>
               <td>2024-11-23</td>
             </tr>
             <tr className="light">
               <td>3</td>
               <td>엄청 예뻐요</td>
               <td>토끼 커플텀블러-플라스틱 프리챌린지</td>
-              <td>witch0817</td>
+              <td>guswl0817</td>
               <td>2024-11-23</td>
             </tr>
           </tbody>
