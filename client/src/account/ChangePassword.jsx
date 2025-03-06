5deb9b079da8b1ecf345a9b969d6f52a4e587dc4
hyeonji817 <guswl930817@gmail.com>
2025-03-06 23:18:12 +0900

    비밀번호 변경 페이지 코드 생성

---
+++ client/src/account/ChangePassword.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react"; 
+import axios from "axios";
+import logo from "../assets/recypro_logo.png";
+import "./ChangePassword.css";
+import Button from "../pages/Button";
