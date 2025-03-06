5f69db08ac5f0445f7d16ca3fe27f2a8abc5d4fb
hyeonji817 <guswl930817@gmail.com>
2025-03-07 01:04:46 +0900

    비밀번호 변경 페이지 일부 구현 및 App.jsx 등록

--- client/src/account/ChangePassword.jsx
+++ client/src/account/ChangePassword.jsx
@@ -3,3 +3,7 @@ import axios from "axios";
 import logo from "../assets/recypro_logo.png";
 import "./ChangePassword.css";
 import Button from "../pages/Button";
+
+const ChangePw = () => {};
+
+export default ChangePw;
