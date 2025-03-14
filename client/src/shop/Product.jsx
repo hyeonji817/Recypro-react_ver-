f050dcd1450e9d81f754d8a1f74b6a14e03e8731
hyeonji817 <guswl930817@gmail.com>
2025-03-15 01:39:33 +0900

    Product.jsx 페이지 코드 UI 기능 구현

--- client/src/shop/Product.jsx
+++ client/src/shop/Product.jsx
@@ -11,183 +11,169 @@ const Product = () => {
 
   useEffect(() => {
     // 서버에서 상품 정보를 가져옴 
-    const fetchProduct = async () => {};
-  })
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5003/api/product/${id}`);  // 서버 API 호출
+        setProduct(response.data);  // 서버에서 받은 데이터로 상태 업데이트
+      } catch (error) {
+        if (error.response) {
+          console.error("상품 정보를 불러오는 데 실패했습니다:", error.response.data);
+        } else {
+          console.error("상품 정보를 불러오는 데 실패했습니다:", error.message);
+        }
+      }
+    };
+
+    fetchProduct();   // 상품 정보 가져오기 호출 
+  }, [id]);
+
+  if (!product) {
+    return <div>상품 정보를 불러오는 중입니다...</div>;
+  }
+
+  console.log("검색한 상품 ID:", id);
+  console.log("이미지 파일명:", product.filename);
 
   return (
     <div className="Product_wrap">
       <div className="Product_Header">
         <Header_loginOK />
       </div>
-      <div className="title">
-        <div className="container-title">
-          <h1 className="display-3">상품 정보</h1>
+      <div className="product-detail">
+        <div className="Product_title">
+          <h2>상품 정보</h2>
         </div>
-      </div>
-      <div className="container">
         <div className="row">
+          {/* 상품 이미지 */}
           <div className="col-md-5">
             <span className="pd_img">
-              <img src={soap} alt="유기농 설거지 비누"/>
+              <img
+                alt="상품 이미지"
+                src={`http://localhost:5001/uploads/shopdetail/${product.filename}`}
+                style={{ width: "100%" }}
+              />
             </span>
           </div>
+
+          {/* 상품 정보 */}
           <div className="col-md-6">
             <div className="sub_explain">
               <span className="pname">
-                <b><h3>유기농 설거지 비누</h3></b>
+                <b>
+                  <h3>{product.pname}</h3>
+                </b>
               </span>
               <span className="desc">
                 <h5>
-                  매일 사용하는 세제를 바꾸는 것만으로도 <br />
-                  자연과 나와 이웃이 행복해집니다.
+                  <p>{product.description}</p>
                 </h5>
               </span>
               <p>
                 <b>상품 코드 : </b>
-                <Button 
-                  text={"가치솝"}
-                  type={"badge-danger"}
-                  className="badge-danger"
-                />
+                <span className="badge badge-danger">{product.productId}</span>
               </p>
               <p>
                 <b>제조사 : </b>
-                라라어스
+                {product.manufacturer}
               </p>
               <p>
                 <b>분류 : </b>
-                욕실
+                {product.category}
               </p>
               <p>
                 <b>재고 수 : </b>
-                100
+                {product.numberOfstock}
               </p>
               <h4>
-                <b>가격 : </b> 3,000원
+                <b>가격 : {product.price} 원</b>
               </h4>
             </div>
-            <p className="button" />
-            <form>
-              <div className="product_bt">
-                <Button 
-                  text={"장바구니 담기"}
-                  type={"cart"}
-                  className="cart_into"
-                />
-                <Button 
-                  text={"상품 목록"}
-                  type={"pdList"}
-                  className="pd_list"
-                />
-                <Button 
-                  text={"리뷰확인"}
-                  type={"review_view"}
-                  className="review_view"
-                />
-              </div>
-            </form>
-            <form>
-              <div className="product_bt2" id="order">
-                <Button 
-                  text={"주문하기"}
-                  type={"order"}
-                  className="order"
-                />
-                <Button 
-                  text={"주문내역 조회"}
-                  type={"pd_orderlist"}
-                  className="pd_orderlist"
-                />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className="membership">
-        <div className="mb_title">
-          <h2>Membership</h2>
-        </div>
-        <div className="membership_events" id="event">
-          <ul>
-            <li>
-              <div className="coupon_give">
-                <div className="new_member_give">
-                  <div className="new_member_give_title">
-                    <h6>신규회원 즉시발급</h6>
-                  </div>
-                  <div className="pay">
-                    <h5>10,000</h5>
-                  </div>
-                </div>
-                <div className="direct_give">
-                  <a>
-                    <h4>쿠폰 즉시발급</h4>
+
+            {/* 버튼 영역 */}
+            <p className="button">
+              <form name="addForm" action={`/addCart/${product.productId}`} method="post">
+                <div className="product_bt">
+                  <a
+                    className="btn btn-default btn-lg"
+                    style={{ backgroundColor: "#b6f5f5", marginRight: "3%" }}
+                    href="/cart"
+                  >
+                    장바구니 담기
+                  </a>
+                  <a
+                    href="/products"
+                    className="btn btn-default btn-lg"
+                    style={{ backgroundColor: "#b6f5f5", marginRight: "3%" }}
+                  >
+                    상품 목록
+                  </a>
+                  <a
+                    href="/productReview"
+                    className="btn btn-default btn-lg"
+                    style={{ backgroundColor: "#b6f5f5" }}
+                  >
+                    리뷰확인
                   </a>
                 </div>
-                <div className="coupon_event">
-                  <h6>
-                    신규회원 <br /> 10,000 + 30% 할인쿠폰
-                  </h6>
+              </form>
+
+              <form name="addForm2" action={`/addOrder/${product.productId}`} method="post">
+                <div className="product_bt" id="order">
+                  <a
+                    className="btn btn-default btn-lg"
+                    style={{ backgroundColor: "#b6f5f5", marginBottom: "-20px;" , marginRight: "3%" }}
+                    href="/order"
+                  >
+                    주문하기
+                  </a>
+                  <a
+                    href="/orderList"
+                    className="btn btn-default btn-lg"
+                    style={{ backgroundColor: "#b6f5f5", marginBottom: "-120px;" }}
+                  >
+                    주문내역 조회
+                  </a>
                 </div>
-              </div>
-            </li>
-            <li>
-              <div className="star">
-                <a>
-                  <img src={star} alt="별" />
-                </a>
-              </div>
-              <a href="#">
-                <span className="level">
-                  <h4>등급별 혜택</h4>
-                </span>
-                <span className="level_event">
-                  <h6>
-                    할인/적립 <br />
-                    무료배송 및 배송비 할인 <br />
-                    생일쿠폰 지급 
-                  </h6>
-                </span>
-              </a>
-            </li>
-            <li>
-              <div className="free_shipment">
-                <a href="#">
-                  <span className="truck">
-                    <img src={shipment} alt="배송" />
-                  </span>
-                  <span className="shipment_title">
-                    <h4>무료배송</h4>
-                  </span>
-                  <span className="shipment_event">
+              </form>
+            </p>
+          </div>
+        </div>
+
+        {/* 회원 이벤트 배너 */}
+        <div className="membership">
+          <div className="mb_title">
+            <h2>Membership</h2>
+          </div>
+          <div className="membership_events" id="event">
+            <ul>
+              <li>
+                <div className="coupon_give">
+                  <div className="new_member_give">
+                    <div className="new_member_give_title">
+                      <h6>신규회원 즉시발급</h6>
+                    </div>
+                    <div className="pay">
+                      <h5>10,000</h5>
+                    </div>
+                  </div>
+                  <div className="direct_give">
+                    <a href="#">
+                      <h4>쿠폰 즉시발급</h4>
+                    </a>
+                  </div>
+                  <div className="coupon_event">
                     <h6>
-                      6만원 이상 <br />
-                      구매 시, <br />
-                      무료배송
+                      신규회원 <br /> 10,000 + 30% 할인쿠폰
                     </h6>
-                  </span>
-                </a>
-              </div>
-            </li>
-            <li>
-              <div className="new_event">
-                <a href="#">
-                  <span className="alarm_event">
-                    <img src={alarm} alt="알림설정" />
-                  </span>
-                  <span className="new_update">
-                    <h4>신상 UP TO</h4>
-                  </span>
-                  <span className="new_discount">
-                    <h6>
-                      매일 업데이트 <br />
-                      30% 할인 지급
-                    </h6>
-                  </span>
-                </a>
-              </div>
-            </li>
-          </ul>
+                  </div>
+                </div>
+              </li>
+              {/* Other Membership Events */}
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
