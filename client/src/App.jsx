699932ea7ad1f1eb567681b4d5f2dc4b9de06928
hyeonji817 <guswl930817@gmail.com>
2025-03-15 23:25:55 +0900

    App.jsx 환경설정 변경

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
         <Route path="/find_pw" element={<FindPw />} />
         <Route path="/ChangePassword" element={<ChangePassword />} />
         <Route path="/shop/Products" element={<Products />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/shoppingInfo_cart" element={<ShoppingInfo_cart />} />
         <Route path="/orderConfirmation" element={<OrderConfirmation />} />
