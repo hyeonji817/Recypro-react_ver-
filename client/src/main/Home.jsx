f455118aa2712da50ab672dbe2c41b0a5d72668c
hyeonji817 <guswl930817@gmail.com>
2025-06-14 23:22:24 +0900

    로그인 이전 메인페이지(Home.jsx) UI 구조 재편성

--- client/src/main/Home.jsx
+++ client/src/main/Home.jsx
@@ -1,23 +1,33 @@
 import "./Home.css";
 import Header from "./Header";
-import Index from "./Index";
-import Popular_pd from "./Popular_pd";
-import Shop_event from "./shop_event";
+import Main_Banner1 from "./Main_Banner1";
+import Main_Banner2 from "./Main_Banner2";
+import Main_Bottom from "./Main_Bottom";
+import BestProducts from "./BestProducts";
+
 import Footer from "./Footer";
 
 const Home = () => {
   return (
-    <div className="wrapper">
-      <Header />
-      {/* 배너 컴포넌트 */}
-      <Index />
-
-      {/* 인기 상품 섹션 */}
-      <Popular_pd />
-
-      {/* 쇼핑 이벤트 섹션 */}
-      <Shop_event />
-      <Footer />
+    <div className="wrap_Home">
+      <div className="Header">
+        <Header />
+      </div>
+      <div className="main_Banner1">
+        <Main_Banner1 />
+      </div> 
+      <div className="main_Banner2">
+        <Main_Banner2 />
+      </div>
+      <div className="bestProducts">
+        <BestProducts />
+      </div>
+      <div className="main_Bottom">
+        <Main_Bottom />
+      </div> 
+      <div className="Footer">
+        <Footer />
+      </div>    
     </div>
   );
 };
