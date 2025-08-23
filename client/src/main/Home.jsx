2ad718d9e0f71aeee59d2346ca98aa4237578f6d
hyeonji817 <guswl930817@gmail.com>
2025-08-23 18:31:05 +0900

    Home.jsx 페이지 코드 일부 추가

--- client/src/main/Home.jsx
+++ client/src/main/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css";
 import Header from "./Header";
+import NewProducts from "./newProducts";
 import Main_Banner1 from "./Main_Banner1";
 import Main_Banner2 from "./Main_Banner2";
 import Main_Bottom from "./Main_Bottom";
@@ -16,6 +17,9 @@ const Home = () => {
       <div className="main_Banner1">
         <Main_Banner1 />
       </div> 
+      <div className="newProducts">
+        <NewProducts />
+      </div>
       <div className="main_Banner2">
         <Main_Banner2 />
       </div>
