7b53e1fa9ca092b20694c7e5d652095a390901e1
hyeonji817 <guswl930817@gmail.com>
2025-09-09 17:10:50 +0900

    Customer_main.jsx/css 페이지 1차 코드구현

--- client/src/customer/Customer_main.jsx
+++ client/src/customer/Customer_main.jsx
@@ -1,81 +1,139 @@
 import "./Customer_main.css";
-import Button from "../pages/Button";
 import Header_loginOK from "../main/Header_loginOK";
-import microphone from "../assets/shop/microphone.png";
-import policy from "../assets/shop/privacy_policy.png";
-import question1 from "../assets/shop/question_1.png";
-import headset1 from "../assets/shop/headset_1.png";
-import declare from "../assets/shop/claim.png"; 
+import Footer from "../main/Footer";
+import btnSearch from "../assets/btn_search.jpg";
+import { useEffect, useState } from "react";
+import btnWrite from "../assets/btn_write.jpg";
+import axios from "axios"; // axios 설치 필요
 
 const Customer_main = () => {
+	const [notices, setNotices] = useState([]);
+	//const isAdmin = user.id === "admin"; // 또는 운영자 조건
+
+	const [newNotice, setNewNotice] = useState({
+		nt_title: "",
+		id: "recypro운영자",
+		date: new Date().toISOString().slice(0, 10),
+		views: 0,
+	});
+	
+	const handleSubmit = () => {
+		axios.post("http://localhost:5001/notice", newNotice)
+			.then(() => {
+				alert("공지사항 등록 완료");
+				window.location.reload(); // or re-fetch data
+			})
+			.catch((err) => console.error(err));
+	};	
+
+  useEffect(() => {
+    axios.get("http://localhost:5001/api/notice")
+      .then((res) => setNotices(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="CustomerMain_wrapper">
-      <div className="Customer_Header">
+      {/** 헤더부분 */}
+      <div className="cnt_Header">
         <Header_loginOK />
       </div>
-      <div className="customer_title">
-        <div className="container">
-          <h1 className="display-3">고객센터</h1>
-        </div>
-      </div>
-      <div className="sub_content">
-        <div className="sub_content_in">
-          <div className="sub_title" id="tc">
-            <h2>고객센터에서 궁금하신 점을 찾아보세요</h2>
-          </div>
-          <div className="sub_title2" id="tc">
-            <h3>궁금하신 점 또는 상품에 대한 질문 내용을 입력해 주시면 상세히 안내해 드립니다.</h3>
-          </div>
-          {/** 고객센터 문의 항목 */}
-          <div className="consumer_info">
-            <ul className="consumer_list1">
-              <li>
-                <div className="notice">
-                  <a href="#">
-                    <img src={microphone} title="공지사항" id="notice_misc" />
-                  </a>
-                  <div className="notice_text">공지사항</div>
-                </div>
-              </li>
-              <li>
-                <div className="privacy_law">
-                  <a href="#">
-                    <img src={policy} title="운영정책" id="policy" />
-                  </a>
-                  <div className="policy_text">운영정책</div>
-                </div>
-              </li>
+      {/** 중앙 */}
+	    <div id="cnt">
+		    <div className="cntbody">
+		      <h2 className="subtitle">공지사항</h2>
+          <div id="boardlist">
+	          <table cellSpacing={0} cellPadding={0} className="board_col">
+		          <caption className="hidden">게시판 글목록</caption>
+		          <colgroup>
+			          <col style={{ width: '8%' }} />
+			          <col />
+			          <col style={{ width: '10%' }} />
+			          <col style={{ width: '12%' }} />
+			          <col style={{ width: '8%' }} />
+		          </colgroup>
+		          <thead>
+			          <tr>
+				          <th scope="col">번호</th>
+				          <th scope="col">제목</th>
+				          <th scope="col">작성자</th>
+				          <th scope="col">작성일</th>
+				          <th scope="col">조회수</th>
+			          </tr>
+		          </thead>
+
+		          <tbody>
+  							{notices.map((notice) => (
+    							<tr className="notice_row" key={notice.nt_num}>
+      							<td className="no">공지</td>
+      							<td className="left">
+        							<strong><a href="#">{notice.nt_title}</a></strong>
+      							</td>
+      							<td>{notice.id}</td>
+      							<td>{notice.date?.slice(0, 10)}</td>
+      							<td>{notice.views}</td>
+    							</tr>
+  							))}
+							</tbody>
+	          </table>
+
+	          <div className="btn_write">
+              <a>
+                <img src={btnWrite} alt="글쓰기" />
+              </a>
+            </div>
+	          <ul className="paging">
+              <li></li>
+              <li><strong>1</strong></li>
+              <li></li>
             </ul>
-            <ul className="consumer_list2">
-              <li>
-                <div className="question">
-                  <a href="#">
-                    <img src={question1} title="자주 묻는 질문" id="question" />
-                  </a>
-                  <div className="question_text">자주 묻는 질문</div>
-                </div>
-              </li>
-              <li>
-                <div className="consult">
-                  <a href="#">
-                    <img src={headset1} title="온라인 상담/문의" id="consult" />
-                  </a>
-                  <div className="consult_text">온라인 상담 / 문의</div>
-                </div>
-              </li>
-              <li>
-                <span className="claim">
-                  <a href="#">
-                    <img src={declare} title="신고하기" id="claim" />
-                    <div className="claim_text">신고하기</div>
-                  </a>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+	          <div className="board_search">
+		          <form method="get">
+                <input type="hidden" name="cate" value="" readOnly />
+                <input type="hidden" name="db" value="basic_1" readOnly />
+
+			          <select name="search">
+				          <option value="title">제목</option>
+				          <option value="content">내용</option>
+				          <option value="name">작성자</option>
+			          </select>
+			          <input type="text" name="search_str" className="input boardsearch" />
+			          <input type="image" src={btnSearch} alt="검색" className="input boardClue" />
+		          </form>
+	          </div>    {/** board_search end */}
+						{/** {isAdmin && ( 
+  						<div className="notice_form">
+    						<input
+      						type="text"
+      						placeholder="공지 제목"
+      						value={newNotice.nt_title}
+      						onChange={(e) => setNewNotice({ ...newNotice, nt_title: e.target.value })}
+    						/>
+    						<button onClick={handleSubmit}>공지 작성</button>
+  						</div>
+						)} */}
+
+
+          </div>    {/** board_list end */}
+          
+          <form name="mariFrm" method="get" action="?" style={{ margin: 0 }}>
+            <input type="hidden" name="db" value="basic_1" readOnly />
+            <input type="hidden" name="no" value="" readOnly />
+            <input type="hidden" name="mari_mode" value="" readOnly />
+            <input type="hidden" name="cate" value="" readOnly />
+            <input type="hidden" name="page" value="1" readOnly />
+            <input type="hidden" name="search" value="" readOnly />
+            <input type="hidden" name="search_str" value="" readOnly />
+            <input type="hidden" name="temp" value="" readOnly />
+          </form>
+		    </div>    {/** cntbody end */}
+	    </div>    {/** cnt end */}
+	    {/** //중앙 */}
+      {/** 푸터 */}
+      <div className="cnt_Footer">
+        <Footer />
       </div>
-    </div>
+    </div>    
   );
 };
 
