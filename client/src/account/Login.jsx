66d81cb1ad1e60ff56c76443d0abc64edc45c357
hyeonji817 <guswl930817@gmail.com>
2025-09-12 14:43:43 +0900

    Login.jsx/css 페이지 코드수정

--- client/src/account/Login.jsx
+++ client/src/account/Login.jsx
@@ -1,117 +1,176 @@
+import { useState } from "react";
 import "./Login.css";
-import { useState } from "react"; 
-import { useNavigate } from "react-router-dom"; 
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../pages/Button";
 
 const Login = () => {
-  const [formData, setFormData] = useState({ id: "", password: "" });
-  const [errorMessage, setErrorMessage] = useState("");
-  const navigate = useNavigate();
+  // formData 및 상태 선언
+const [formData, setFormData] = useState({ id: "", password: "" });
+const [errorMessage, setErrorMessage] = useState("");
+const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormData({ ...formData, [name]: value });
+};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("로그인 요청 보냄:", formData); 
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:5003/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+  try {
+    const response = await fetch("http://localhost:5001/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include", // 세션 쿠키 포함
+      body: JSON.stringify(formData),
+    });
 
-      console.log("서버 응답 수신:", response);
+    const data = await response.json();
 
-      const data = await response.json();
-      console.log("서버 응답 데이터:", data);
+    if (!response.ok) {
+      setErrorMessage(data.message || "로그인 실패");
+    } else {
+      alert(data.message);
+      localStorage.setItem("token", data.token); // 필요 시 JWT 저장
+      alert("로그인 성공!");
 
-      if (!response.ok) {
-        setErrorMessage(data.message || "로그인 실패"); 
+      if (formData.id === "admin" && formData.password === "1234") {
+        navigate("/login_admin");
       } else {
-        alert(data.message); 
-        // 토큰 저장 (ex) 로컬 스토리지)
-        localStorage.setItem("token", data.token);
-
-        // navigate를 useEffect에서 실행 
-        setTimeout(() => {
-          window.location.href = "/index";
-        }, 100);
+        navigate("/index");
       }
-    } catch (error) {
-      console.error("로그인 요청 실패:", error); 
-      setErrorMessage("서버와의 통신에 문제가 발생했습니다.");
     }
-  };
+  } catch (err) {
+    console.error("서버 오류:", err);
+    setErrorMessage("서버 통신 오류");
+  }
+};
+
 
   return (
     <div className="wrapper_login">
       <div className="login">
         <h1 className="logo">
-          <a href="../main/Home.jsx">리싸이프로</a>
+          <Link className="logo" to="/home">리싸이프로</Link>
         </h1>
-        <form onSubmit={handleSubmit}>
-          <br /><br /><br /><br />
-          <h1 className="login_title">로그인</h1>
-          <br />
-        </form>
-        <div className="login_body">
-          <form>
-            <div className="form-group">
-              <input 
-                type="text"
-                name="id"
-                id="id"
-                className="form-control"
-                placeholder="아이디"
-                value={formData.id}
-                onChange={handleChange}
-                required
-                autoFocus
-              />
-            </div>
-            <div className="form-group">
-              <input 
-                type="password"
-                name="password"
-                id="password"
-                className="form-control"
-                placeholder="비밀번호"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-            <Button 
-              text={"로그인"}
-              type={"login"}
-              className="login-button" 
-              onClick={handleSubmit}
-            />
-            <div className="text-center">
-              <br />
-              <a href="./register" className="d-block">회원가입하기</a>
-              <a href="/find_id" className="d-block">아이디 찾기</a>
-              <a href="/find_pw" className="d-block">비밀번호 찾기</a>
-            </div>
+        
+        {/** 추가된 내용 */}
+        <div id="login" className="box_member login">
+	        <div className="login_wrap">
+		        <div className="login_box">
+			        <div className="tabcnt_member tabcnt_member0" style={{ display: "block" }}>	
+				        <form method="post" onSubmit={handleSubmit} style= {{ margin: "0px" }}>
+                  <input type="hidden" name="exec_file" value="member/login.exe.php" />
+                  <input type="hidden" name="rURL" value="https://www.rolarola.com/main/index.php" />
+                  <input type="hidden" name="urlfix" value="Y" />
 
-            <div className="button-login">
-              <a>
-                <img 
-                  src="//k.kakaocdn.net/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg"
-                  alt="카카오계정 로그인"
-                />
-              </a>
-              <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-            </div>
-          </form>
-        </div>
+					        <fieldset>
+						        <legend className="hidden">회원 로그인</legend>
+						        <div className="fld top">
+							        <label htmlFor="login_id">ID</label>
+							        <input 
+                        type="text" 
+                        name="id" 
+                        value={formData.id} 
+                        onChange={handleChange} 
+                        maxLength={50} 
+                        id="login_id" 
+                        className="form_input member block" 
+                      />
+							        <div className="auto_complete auto_complete_member_id"></div>
+					  	      </div>    {/** fld top end */}
+
+						        <div className="fld">
+							        <label htmlFor="login_pwd">PASSWORD</label>
+							        <input 
+                        type="password" 
+                        name="password" 
+                        value={formData.password} 
+                        onChange={handleChange} 
+                        id="login_pwd" 
+                      className="form_input member block" />
+							        <span className="show_icon"></span>
+						        </div>    {/** fld end */}
+
+						        <div className="savessl">
+							        <input type="checkbox" name="setOpenSSL" id="member_ssl" checked="" />
+                      <label htmlFor="member_ssl">보안접속</label>
+							        <input type="checkbox" name="id_save" value="Y" id="member_id_save" />
+                      <label htmlFor="member_id_save">아이디저장</label>
+							        <input type="checkbox" name="pw_save" value="Y" id="member_pwd_save" />
+                      <label htmlFor="member_pwd_save">비밀번호저장</label>
+						        </div>  {/** savessl end */}
+
+						        <div className="login_btn_wrap">
+							        <span className="box_btn block black2">
+                        <input type="submit" value="LOG IN" />
+                      </span>
+							        <span className="box_btn w144 gray3"></span>
+						        </div>    {/** login_btn_wrap end */}
+
+						        <div className="joinfind">
+                      <p><Link to="/account/register">CREATE ACCOUNT</Link></p>
+							        <p><a href="#">FIND ID</a></p>
+                      <p><a href="#">FIND PASSWORD</a></p>
+						        </div>    {/** joinfind end */}
+					        </fieldset>
+				        </form>
+
+				        <div className="sns_wrap">
+					        <div className="simple">
+						        <a href="#" className="kakao">카카오로 시작하기</a>
+						        <a href="#" className="naver">네이버로 시작하기</a>
+						        <a href="#" className="apple">애플로 시작하기</a>
+					        </div>    {/** simple end */}
+				        </div>    {/** sns_wrap end */}
+			        </div>      {/** tabcnt_member tabcnt_member0 end */}
+
+			        <div className="tabcnt_member tabcnt_member1 tab_order" style={{ display: "none" }}>
+				        <form method="post" action="https://www.rolarola.com/mypage/order_detail.php" style={{ margin: "0px" }}>
+                  <input type="hidden" name="exec" value="orderDetail" />
+
+					        <fieldset>
+						        <legend className="hidden">비회원 주문조회</legend>
+						        <div className="fld top">
+							        <label htmlFor="login_order_no">ORDER NO</label>
+							        <input type="text" name="ono" id="login_order_no" className="form_input member block" />
+						        </div>    {/** fld top end */}
+						        <div className="fld">
+							        <label htmlFor="login_phone">PHONE NO</label>
+							        <input type="text" name="phone" id="login_phone" className="form_input member block" />
+						        </div>    {/** fld end */}
+						        <div className="savessl dn">
+							        <input type="checkbox" name="setOpenSSL" id="nonmember_ssl" checked="" /> 
+                      <label htmlFor="nonmember_ssl">보안접속</label>
+						        </div>    {/** savessl dn end */}
+						        <div className="login_btn_wrap">
+							        <span className="box_btn block black2">
+                        <input type="submit" value="OK" />
+                      </span>
+						        </div>    {/** login_btn_wrap end */}
+					        </fieldset>
+				        </form>
+			        </div>    {/** tabcnt_member tabcnt_member1 tab_order end */}
+			
+			        <ul className="tab_member">
+				        <li className="first">
+                  <a className="active">회원 로그인</a>
+                </li>
+				        <li><a href="#">비회원 주문조회</a></li>
+			        </ul>
+		        </div>    {/** login_box end */}
+	        </div>    {/** login_wrap end */}
+        </div>    {/** box_member login end */}
+
+        {/** <div className="button-login">
+          <a>
+            <img 
+              src="//k.kakaocdn.net/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg"
+              alt="카카오계정 로그인"
+            />
+          </a>
+          <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+        </div> */}
       </div>
     </div>
   );
