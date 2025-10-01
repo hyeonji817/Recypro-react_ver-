af39bbfcc459a8410cd3585eb4ca1b82c66483ee
hyeonji817 <guswl930817@gmail.com>
2025-10-02 00:25:21 +0900

    Product1.jsx - 상태값 선언

--- client/src/shop/Product1.jsx
+++ client/src/shop/Product1.jsx
@@ -16,6 +16,22 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 
-const Product1 = () => {};
+const Product1 = () => {
+  const { productId } = useParams(); 		// 상품명 호출
+	console.log("productId param = ", productId);
+	console.log("[Product1] productId =", productId);
+	const nav = useNavigate(); 
+	const [product, setProduct] = useState(null);
+	const [loading, setLoading] = useState(true); 
+	const [error, setError] = useState("");
+	const [selected, setSelected] = useState({});		// 객체로 선언, optionGroups.map()에서 selected[group.name]을 접근하기 때문에, selected가 객체여야 동작함.
+	const [selectedColor, setSelectedColor] = useState("");
+	// 계산 상태
+	const [qty, setQty] = useState(1);		// 수량 
+	// Product1 컴포넌트 내부 (state 선언들 아래 아무 곳)
+	const groups = React.useMemo(() => product?.optionGroups ?? [], [product]);
+
+  
+};
 
 export default Product1;
