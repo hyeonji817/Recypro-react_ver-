f7e08ab5a01737c7ec461f60afa5eb39b4f67976
hyeonji817 <guswl930817@gmail.com>
2025-10-01 23:52:13 +0900

    Product1.jsx - Product1 컴포넌트 뼈대구현

--- client/src/shop/Product1.jsx
+++ client/src/shop/Product1.jsx
@@ -9,3 +9,13 @@ import axios from "axios";
 import Header_loginOK from "../main/Header_loginOK";
 import Footer from "../main/Footer";
 import CouponBanner from "../assets/coupon_banner.jpg";
+
+// Swiper 스타일 (필수)
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+
+const Product1 = () => {};
+
+export default Product1;
