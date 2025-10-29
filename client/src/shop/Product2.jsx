51c289a837fa8d7e523728dbc6566c458faee3a2
hyeonji817 <guswl930817@gmail.com>
2025-10-29 21:51:52 +0900

    Product2.jsx - 구매하기, 장바구니 담기 버튼 구현

--- client/src/shop/Product2.jsx
+++ client/src/shop/Product2.jsx
@@ -343,7 +343,17 @@ const Product2 = () => {
   											</div>
 											</div> 
 
-											
+											{/** 버튼 */}
+					            {/** ★★★★ 모듈 ★★★★ */}
+											<div className="btn">
+						            <span className="box_btn large buy block">
+                          <a href="/orderList" onClick={(e)=>{ e.preventDefault(); alert("바로구매(추후연결)"); }}>BUY NOW</a>
+                        </span>   {/** box_btn large buy block end */}
+						            <span className="box_btn large cart block">
+                          <a href="/cart" onClick={(e)=>{ e.preventDefault(); handleAddToCart(); }}>ADD TO BAG</a>
+                        </span>   {/** box_btn large cart block end */}
+						          </div>    {/** btn end */}
+					            <div></div>
 
 									</div>		{/** wrap_prd end */}
 								</form>
