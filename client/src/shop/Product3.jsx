cb0f6d42aaf6893d124c137839d3c927141a0aec
hyeonji817 <guswl930817@gmail.com>
2025-11-09 20:09:42 +0900

    Product3.jsx - 상수 선언언

---
+++ client/src/shop/Product3.jsx
@@ -0,0 +1,34 @@
+import "./Product3.css";
+import React, { useEffect, useState } from "react"; 
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
+
+import axios from "axios";
+import Header_loginOK from "../main/Header_loginOK";
+import Footer from "../main/Footer";
+import CouponBanner from "../assets/coupon_banner.jpg";
+
+// Swiper 스타일 (필수)
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+const Product3 = () => {
+  const { productId } = useParams();		// 상품명 호출
+	console.log("productId param = ", productId);
+	console.log("[Product3] productId =", productId);
+	const nav = useNavigate();
+	const [product, setProduct] = useState(null);		// 불러들일 상품 데이터 
+	const [loading, setLoading] = useState(true);		
+	const [error, setError] = useState("");		
+	const [selected, setSelected] = useState({});		// 객체로 선언
+	const [selectedColor, setSelectedColor] = useState("");
+	const [qty, setQty] = useState(1);		// 계산 상태, 수량
+	const groups = React.useMemo(() => product?.optionGroups ?? [], [product]);		// Product3 컴포넌트 내부 (state 선언들 아래 아무 곳에 배치하기)
+
+  
+};
+
+export default Product3;
