b1856970b5a8ea27212c6169b6f72816a38f2d5c
hyeonji817 <guswl930817@gmail.com>
2025-11-17 17:44:08 +0900

    Product4.jsx - 컴포넌트 내 상태 선언언

--- client/src/shop/Product4.jsx
+++ client/src/shop/Product4.jsx
@@ -15,6 +15,22 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
-const Product4 = () => {};
+const Product4 = () => {
+  const { productId } = useParams();		// 상품명 호출
+	console.log("productId param = ", productId);
+	console.log("[Product4] productId =", productId);
+	const nav = useNavigate();
+	const [product, setProduct] = useState(null);		// 불러들일 상품 데이터 
+	const [loading, setLoading] = useState(true);		
+	const [error, setError] = useState("");		
+	const [selected, setSelected] = useState({});		// 객체로 선언
+	const [selectedColor, setSelectedColor] = useState("");
+	// === 계산 상태 ===
+	const [qty, setQty] = useState(1); // 수량
+	// Product4 컴포넌트 내부 (state 선언들 아래 아무 곳)
+	const groups = React.useMemo(() => product?.optionGroups ?? [], [product]);
+
+  
+};
 
 export default Product4;
