5ad49125a851d0bbdeffcc99fafae792b5bfa255
hyeonji817 <guswl930817@gmail.com>
2025-11-19 18:33:14 +0900

    Product4.jsx - 로딩상황 및 DB 컬럼 관련 상수 선언언

--- client/src/shop/Product4.jsx
+++ client/src/shop/Product4.jsx
@@ -133,7 +133,16 @@ const Product4 = () => {
   	return [String(v)];
 	};
 
-	
+	if (loading) return <div className="Product_wrap">Loading...</div>;
+  if (error) return <div className="Product_wrap">{error}</div>;
+  if (!product) return null;
+	if (!productId) return <div className="Product_wrap">잘못된 페이지입니다.(상품 ID 없음)</div>;
+
+	const {
+    pname, price, discount_price, discount_rate,
+    description, manufacturer, numberOfstock, category, filename, mileage, img_Desc,
+		selectColor, productCount,
+  } = product;
 
 };
 
