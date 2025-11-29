1b3fa440a4c9a8b3335dbe6d0de2f9db7967de5e
hyeonji817 <guswl930817@gmail.com>
2025-11-29 21:49:10 +0900

    Product5.jsx - 상품정보 및 버튼 구현현

--- client/src/shop/Product5.jsx
+++ client/src/shop/Product5.jsx
@@ -203,7 +203,32 @@ const Product5 = () => {
 								</div>			{/** related_wrap end */}
 							</div>			{/** prdimg end */}
 
-							
+							{/** 상품 이미지 */}
+							<div className="info_scroll">
+								<form name="prdFrm" method="post" style={{ margin: "0px" }} acceptCharset="utf-8">
+									<div className="wrap_prd">
+										{/** 상품정보 & 버튼 */}
+										<div className="info">
+											<h3 className="name">{pname}</h3>			{/** name end */}
+											<p className="summary">{manufacturer} · {category}</p>			{/** summary end */}
+											<div className="price">
+												<div className="top_price">
+													<span className="consumer consumerY">{price?.toLocaleString()} 원</span>			{/** consumer consumerY end */}
+													<span className="sell sellY">
+														<strong>{discount_price?.toLocaleString()}%</strong>	
+													</span>		{/** sell sellY end */}
+												</div>			{/** top_price end */}
+												<span className="discount discountY">
+													<strong>{discount_price?.toLocaleString()}</strong>	
+												</span>			{/** discount discountY end */}
+												<span className="per">{discount_rate}원</span>		{/** per end */}
+											</div>			{/** price end */}	
+
+											
+										</div>			{/** info end */}	
+									</div>			{/** wrap_prd end */}
+								</form>
+							</div>			{/** info_scroll end */}
 						</div>			{/** detail_top_wrap end */}
 					</div>			{/** detail end */}
 				</div>			{/** detailPd_body end */}
