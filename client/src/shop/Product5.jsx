a9d8a2b1a25175e39b648e84e689adc8304af271
hyeonji817 <guswl930817@gmail.com>
2025-11-30 13:07:42 +0900

    Product5.jsx - 수량 구하는 이벤트 구현현

--- client/src/shop/Product5.jsx
+++ client/src/shop/Product5.jsx
@@ -279,7 +279,16 @@ const Product5 = () => {
 												</div>
 											))}
 
-											
+											{/** 수량 */}
+											<div className="box_qty hidden">
+												<input type="text" name="buy_ea" value={1} className="form_input" />
+												<div className="btn_ea">
+													<a href="#" className="ea_up">+</a>
+													<a href="#" className="ea_down">-</a>
+												</div>			{/** btn_ea end */}
+											</div>			{/** box_qty hidden end */}
+
+
 										</div>			{/** info end */}	
 									</div>			{/** wrap_prd end */}
 								</form>
