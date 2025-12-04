2768f73d38e72edc2c71fe6063d9baa2b2757937
hyeonji817 <guswl930817@gmail.com>
2025-12-04 16:09:37 +0900

    Products_Office.jsx - 상품정렬 내 이미지 구현현

--- client/src/shop/Products_Office.jsx
+++ client/src/shop/Products_Office.jsx
@@ -38,87 +38,51 @@ const Products_Office = () => {
             <li><Link to="/shop/products5">반려동물</Link></li>
             <li><Link to="/shop/products6">사무</Link></li>
           </ul>   {/** sub_category end */}
-          
-          {/** 검색하기 기능 */}
-          <ul className="search-container">
-            {isSearchOpen && (
-            <input
-              type="text"
-              className="search-input"
-              placeholder="상품명을 입력하세요"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            )}
-            <button
-              className="search-toggle"
-              onClick={() => setIsSearchOpen(!isSearchOpen)}
-            >
-              🔍
-            </button>
+
+	        {/** 상품정렬 */}
+          <ul className="prd_basic col3">
+          {currentItems.map((item) => {
+            const detailPath = `/shop/product6/${encodeURIComponent(item.productId)}`;
+            return (
+              <li key={item.productId}>
+                <div className="box">
+                  <div className="img">
+                    <div className="prdimg">
+                      <Link to={detailPath}>
+                        <img
+                          src={`http://localhost:5003/uploads/${item.filename}`}
+                          alt={item.pname}
+                          width={240}
+                          height={320}
+                        />
+                      </Link>  
+                    </div>    {/** prdimg end */}  
+                  </div>     {/** img end */}  
+
+                  
+                </div>     {/** box end */}
+              </li>
+            );
+          })}
           </ul>
 
-	    {/** 상품정렬 */}
-      <ul className="prd_basic col3">
-        {productOffice.map((item, index) => (
-        <li key={index}>
-		      <div className="box ">
-	          <div className="img">
-		          <div className="prdimg">
-                <a href={item.href}>
-                  <img 
-                    src={`http://localhost:5001/uploads/${item.filename}`} 
-                    alt={item.pname}
-                    width="240" 
-                    height="320" 
-                  />
-                </a>
-              </div>  {/** prdimg end */}
-	          </div>  {/** img end */}
-
-	          <div className="info">
-		          <p className="name">
-                <a href={item.href}>
-                  {item.pname}
-                </a>
-              </p>    {/** name end */}
-		          <div className="price">
-								<p className="consumer consumerY">
-                  <span>KRW</span>
-                  {item.price}
-                </p>    {/** consumer consumerY end */}
-			          <p className="sell sellY">
-                  <span>KRW</span>
-                  {item.price}
-                </p>  {/** sell sellY end */}
-			          <div className="discount_section">
-				          <p className="per">{item.discount_rate}%</p>
-				          <p className="discount discountY">{item.discount_price}</p>
-			          </div> {/** discount_section end */}
-		          </div>  {/** price end */}
-	          </div>  {/** info end */}
-          </div>  {/** box end */}
-	      </li>
-        ))}
-      </ul>
-
-	    <ul className="paging">
-        <li></li>
-        <li><strong>1</strong></li>
-        <li><a href="?page=2&amp;cno1=1800">2</a></li>
-        <li><a href="?page=3&amp;cno1=1800">3</a></li>
-        <li><a href="?page=4&amp;cno1=1800">4</a></li>
-        <li><a href="?page=5&amp;cno1=1800">5</a></li>
-        <li><a href="?page=6&amp;cno1=1800">6</a></li>
-        <li><a href="?page=7&amp;cno1=1800">7</a></li>
-        <li><a href="?page=8&amp;cno1=1800">8</a></li>
-        <li><a href="?page=9&amp;cno1=1800">9</a></li>
-        <li><a href="?page=10&amp;cno1=1800">10</a></li>
-        <li><a className="next moveto" href="?page=11&amp;cno1=1800">&gt;</a><a className="last moveto" href="?page=19&amp;cno1=1800">&gt;&gt;</a></li>
-      </ul>
+	        <ul className="paging">
+            <li></li>
+            <li><strong>1</strong></li>
+            <li><a href="?page=2&amp;cno1=1800">2</a></li>
+            <li><a href="?page=3&amp;cno1=1800">3</a></li>
+            <li><a href="?page=4&amp;cno1=1800">4</a></li>
+            <li><a href="?page=5&amp;cno1=1800">5</a></li>
+            <li><a href="?page=6&amp;cno1=1800">6</a></li>
+            <li><a href="?page=7&amp;cno1=1800">7</a></li>
+            <li><a href="?page=8&amp;cno1=1800">8</a></li>
+            <li><a href="?page=9&amp;cno1=1800">9</a></li>
+            <li><a href="?page=10&amp;cno1=1800">10</a></li>
+            <li><a className="next moveto" href="?page=11&amp;cno1=1800">&gt;</a><a className="last moveto" href="?page=19&amp;cno1=1800">&gt;&gt;</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
