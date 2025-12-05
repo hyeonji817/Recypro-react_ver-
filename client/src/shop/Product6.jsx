78769d1813dc8cab741669118d2429ef45946006
hyeonji817 <guswl930817@gmail.com>
2025-12-05 13:02:59 +0900

    Product6.jsx - useState 상태 선언언

--- client/src/shop/Product6.jsx
+++ client/src/shop/Product6.jsx
@@ -15,6 +15,18 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
-const Product6 = () => {};
+const Product6 = () => {
+  const { productId } = useParams();    // 상품명 호출 
+  const nav = useNavigate();
+  const [product, setProduct] = useState(null);   // 불러들일 상품 데이터를 보관하는 상태 보관소 
+  const [loading, setLoading] = useState(true);   // 로딩상태 
+  const [error, setError] = useState("");         // 에러 
+  const [selected, setSelected] = useState({});   // 선택항목 관련 데이터를 보관하는 상태 보관소 
+  const [selectedColor, setSelectedColor] = useState("");     // 선택할 종류 및 색상
+  const [qty, setQty] = useState(1);        // 계산상태, 수량 
+  const groups = React.useMemo(() => product?.optionGroups ?? [], [product]);   
+  console.log("productId param = ", productId);
+  console.log("[Product6] productId = ", productId);
+};
 
 export default Product6;
