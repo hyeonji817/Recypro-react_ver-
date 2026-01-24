50b458f4c633b6d1ac92a93f3ddf8f0ffb8821e2
hyeonji817 <guswl930817@gmail.com>
2026-01-24 19:02:45 +0900

    Product6.jsx - 장바구니 추가, 바로구매 버튼 구현

--- client/src/shop/Product6.jsx
+++ client/src/shop/Product6.jsx
@@ -342,6 +342,18 @@ const Product6 = () => {
     											</strong>
   											</div>    {/** opt_total end */}
                       </div>     {/** multi_opt end */}
+
+                      {/** 버튼 */}
+					            {/** ★★★★ 모듈 ★★★★ */}
+					            <div className="btn">
+						            <span className="box_btn large buy block">
+                          <a href="/orderList" onClick={(e)=>{ e.preventDefault(); alert("바로구매(추후연결)"); }}>BUY NOW</a>
+                        </span>   {/** box_btn large buy block end */}
+						            <span className="box_btn large cart block">
+                          <a href="/cart" onClick={(e)=>{ e.preventDefault(); handleAddToCart(); }}>ADD TO BAG</a>
+                        </span>   {/** box_btn large cart block end */}
+						          </div>    {/** btn end */}
+					            <div></div>
                       
                     </div>    {/** info end */}
                   </div>      {/** wrap_prd end */}  
