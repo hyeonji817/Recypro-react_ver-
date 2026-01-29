07b82d25b3d8f172efc8888b9960b4aea7cb8425
hyeonji817 <guswl930817@gmail.com>
2026-01-29 17:56:40 +0900

    Product_List.jsx - 상태값 및 상수 선언

--- client/src/shop/Product_List.jsx
+++ client/src/shop/Product_List.jsx
@@ -3,7 +3,15 @@ import "./Product_List.css";
 import { useEffect, useState } from "react"; 
 
 const Product_List = () => {
-
+  const [productLists, setProductsList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); 
+  const itemsPerPage = 3;
+  const indexOfLastItem = currentPage * itemsPerPage; 
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = productLists.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(productLists.length / itemsPerPage);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+  
   return (
     <div className="Product_List_wrap">
       
