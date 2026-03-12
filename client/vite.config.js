69a2716515a2dcde40b3e5206ae55d9b5532b178
hyeonji817 <guswl930817@gmail.com>
2026-03-12 23:49:04 +0900

    프론트 포트번호 변경경

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/** export default defineConfig({
   plugins: [react()],   // react 플러그인 활성화 : React 프로젝트를 위한 Vite 플러그인을 사용, JSX 및 Fast Refresh 지원
   resolve: {
     alias: {
@@ -10,6 +10,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: "127.0.0.1",
     port: 5174,   // 개발 서버가 실행될 포트 지정 (기본값을 5174번)
     proxy: {
       '/api': {
@@ -20,4 +21,13 @@ export default defineConfig({
       },
     },
   },
-});
+}); */
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    strictPort: true,
+  },
+})
