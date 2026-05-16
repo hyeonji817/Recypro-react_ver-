85f55d16651748f60f600f3be2c3e91032f2972f
hyeonji817 <guswl930817@gmail.com>
2026-05-16 16:35:23 +0900

    orderList2.jsx - import문 구현

---
+++ client/src/shop/orderList2.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react"; 
+import { useSearchParamas, useNavigate } from "react-router-dom"; 
+import "./orderList2.css";
+import axios from "axios"; 
+import Header_loginOK from "../main/Header_loginOK";
+import Footer from "../main/Footer";
+import PaymentModal from "../components/PaymentModal";
+import beepBeep_Toy1 from "../assets/pet/1. beepBeep_Toy1.jpg";
+
