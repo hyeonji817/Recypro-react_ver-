ef1dda2a859680663d287b3379d3fcde82501eab
hyeonji817 <guswl930817@gmail.com>
2026-05-16 21:57:39 +0900

    orderList2.jsx - 배송지 정보 구현현

--- client/src/shop/orderList2.jsx
+++ client/src/shop/orderList2.jsx
@@ -201,6 +201,241 @@ const OrderList2 = () => {
               </div>     {/** print_receipt end */}
 
               {/** 주문상품정보 */}
+              <table className="tbl_col prd">
+                <caption className="hidden">주문상품</caption>
+                <colgroup>
+                  <col style={{ width: "100px" }} />
+                  <col />
+                  <col style={{ width: "18%" }} />
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </colgroup>
+
+                <thead>
+                  <tr>
+                    <th scope="col" colSpan={2}>상품명</th>
+                    <th scope="col">옵션</th>
+                    <th scope="col">가격</th>
+                    <th scope="col">수량</th>
+                    <th scope="col">총 금액</th>
+                    <th scope="col">적립금</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td className="img">
+                      <a href="#">
+                        <img src={beepBeep_Toy1} width={52} height={70} />
+                      </a>
+                    </td>
+
+                    <td className="tal"><a href="#">[지구pick] 바잇미 업사이클 봉봉그린 반려동물 삑삑이 장난감</a></td>
+                    <td className="tal"> 종류 : 토끼<br /> <div></div></td>
+                    <td>7,110 원</td>
+                    <td>1</td>
+                    <td>7,110 원</td>
+                    <td>158 원</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              {/** //주문상품정보 */}
+              <div className="ord_info">
+                <div className="area_left">
+                  {/** 사은품 */}
+                  <div id="gift_area"></div>
+                  {/** /사은품 */}
+                  <h3 className="title">할인 / 혜택 사용</h3>
+                  <table className="tbl_order">
+                    <caption className="hidden">할인 / 혜택 사용</caption>
+                    <colgroup>
+                      <col style={{ width: "18%" }} />
+                      <col />
+                    </colgroup>
+
+                    <tbody>
+                      <tr>
+                        <th scope="row">적립금 사용</th>
+                        <td>
+                          <input type="text" name="milage_prc" value="0" className="form_input tar" /> 사용 가능 적립금 : <strong className="own_mileage">2,000</strong> 원
+                        </td>
+                      </tr>
+        
+                      {/** 쿠폰사용 */}  
+                      <tr>
+                        <th scope="row">전체상품 쿠폰</th>
+                        <td>
+                          <ul className="coupon_list">
+                            <li>
+                              <span className="check">
+                                <input type="hidden" id="coupon_stype_1038523" value="1" />
+                                <input type="hidden" name="coupon_pay_type" value="1" />
+                                <input type="radio" name="coupon" id="coupon" value="1038523" />
+                              </span>   {/** check end */}
+                              <p className="name">[온라인전용] 멤버십_5천 원 할인 쿠폰</p>
+                              <p className="content">
+                                최소주문금액 : 30,000 원 이상
+                                / 할인액(율) : 5,000원 
+                                / 사용기간 :  ~ 2025-07-31
+                              </p>    {/** content end */}
+                            </li>
+
+                            <li>
+                              <span className="check">
+                                <input type="hidden" id="coupon_stype_848011" value="1" />
+                                <input type="hidden" name="coupon_pay_type" value="1" />
+                                <input type="radio" name="coupon" id="coupon" value="848011" />
+                              </span>   {/** check end */}
+                              <p className="name">[온라인전용] 멤버십_10% 할인 쿠폰</p>
+                              <p className="content">
+                                최소주문금액 : 10,000 원 이상
+                                / 할인액(율) : 10% 
+                                / 사용기간 :  ~ 2025-07-31
+                                <br />최대할인금액 1,000,000 원
+                              </p>    {/** content end */}
+                            </li>
+
+                            <li>
+                              <span className="check">
+                                <input type="radio" id="no_cpn" name="coupon" value="" />
+                              </span>
+                              <p className="name">사용안함</p>
+                              <p className="content"></p>
+                            </li>
+                          </ul>   {/** coupon_list end */}
+                        </td>
+                      </tr>
+                      {/** // 쿠폰사용 */}
+                    </tbody>
+                  </table>    {/** tbl_order end */}
+    
+                  <h3 className="title">주문자 입력</h3>
+                  <table className="tbl_order">
+                    <caption className="hidden">주문자 입력</caption>
+                    <colgroup>
+                      <col style={{ width: "18%" }} />
+                      <col />
+                    </colgroup>
+
+                    <tbody>
+                      <tr>
+                        <th scope="row"><label htmlFor="order_buyer_name">주문하시는 분</label></th>
+                        <td>
+                          <input type="text" name="buyer_name" value="곽현지" id="order_buyer_name" className="form_input" />
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row"><label htmlFor="buyer_phone">전화번호</label></th>
+                        <td>
+                          <input type="text" name="buyer_phone" id="buyer_phone" value="" className="form_input remove_dash" />
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row"><label htmlFor="buyer_cell">휴대전화번호</label></th>
+                        <td>
+                          <input type="text" name="buyer_cell" id="buyer_cell" value="01094398468" className="form_input remove_dash" />
+                          <input type="checkbox" name="sms" id="sms" value="Y" checked="" />
+                          <label htmlFor="sms" className="msg">주문관련 SMS를 수신합니다.</label>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row"><label htmlFor="order_buyer_email">이메일</label></th>
+                        <td>
+                          <input type="text" name="buyer_email" value="narimjoon@naver.com" id="order_buyer_email" className="form_input mail3" />
+                        </td>
+                      </tr>
+                      {/** 주문서 - 회원주문일때, 회원정보 수정 링크 */}
+                      <tr>
+                        <td colSpan={2} className="edit">
+                          회원정보가 변경되셨다면 다음 버튼을 누르고 수정해 주세요. 
+                          <span className="box_btn small">
+                            <a href="#">회원정보수정</a>
+                          </span>   {/** box_btn small end */}
+                        </td>   {/** edit end */}
+                      </tr>
+                      {/** //주문서 - 회원주문일때, 회원정보 수정 링크 */}
+                    </tbody>
+                  </table>
+
+                  <div className="title_delivery">
+                    <h3 className="title">배송지 정보</h3>
+                    <label className="msg">
+                      <input type="checkbox" name="save_addr" value="Y" /> 
+                      현재 배송지 정보를 회원정보로 저장
+                    </label>    {/** msg end */}
+                  </div>    {/** title_delivery end */}
+
+                  <table className="tbl_order">
+                    <caption className="hidden">배송지 정보</caption>
+                    <colgroup>
+                      <col style={{ width: "18%" }} />
+                      <col />
+                    </colgroup>
+
+                    <tbody>
+                      <tr>
+                        <th scope="row">기존 배송지</th>
+                        <td>
+                          <select name="old_addr_sel">
+                            <option value="">새로운주소 입력</option>
+                            <option value="곽현지&lt;wisamall&gt;&lt;wisamall&gt;01094398468&lt;wisamall&gt;08786&lt;wisamall&gt;서울특별시 관악구 청룡3길 10(봉천동)&lt;wisamall&gt;영빌딩 603호&lt;wisamall&gt;&lt;wisamall&gt;&lt;wisamall&gt;&lt;wisamall&gt;곽현지&lt;wisamall&gt;15760&lt;wisamall&gt;Y" selected="">[기본 배송지] 곽현지 : 서울특별시 관악구 청룡3길 10(봉천동) 영빌딩 603호</option>
+                          </select>
+                          <label className="msg">
+                            <input type="checkbox" name="copy_info" /> 주문인 정보와 동일
+                          </label>    {/** msg end */}
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row"><label htmlFor="order_addressee_name">받으시는 분</label></th>
+                        <td>
+                          <input type="text" name="addressee_name" value="" id="order_addressee_name" className="form_input" />
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row"><label htmlFor="addressee_phone">전화번호</label></th>
+                        <td>
+                          <input type="text" name="addressee_phone" id="addressee_phone" className="form_input remove_dash" />
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row"><label htmlFor="addressee_cell">휴대전화번호</label></th>
+                        <td>
+                          <input type="text" name="addressee_cell" id="addressee_cell" className="form_input remove_dash" />
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row"><label htmlFor="order_addressee_zip">주소</label></th>
+                        <td className="address">
+                          <p className="zip">
+                            <input type="text" name="addressee_zip" value="" id="order_addressee_zip" className="form_input input_zipcode" />
+                            <span className="box_btn white"><a href="#"> 우편번호 찾기</a></span>
+                          </p>    {/** zip end */}
+                          <p>
+                            <input type="text" name="addressee_addr1" value="" className="form_input" />
+                            <input type="text" name="addressee_addr2" value="" className="form_input" />
+                          </p>
+                        </td>   {/** address end */}
+                      </tr>
+        
+                      <tr>
+                        <th scope="row"><label htmlFor="order_dlv_memo">배송시요청사항</label></th>
+                        <td>
+                          <textarea type="text" name="dlv_memo" value="" id="order_dlv_memo" className="form_input block"></textarea>
+                        </td>
+                      </tr>
+                      {/** ADDINFO_DONE */}
+                    </tbody>
+                  </table>
+                </div>    {/** area_left end */}
+              </div>    {/** ord_info end */}
+
               
 
             </form>     {/** form end */}
