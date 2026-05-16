9f9c8fe4f8d7c1231a4ab3a45755ee54eb7ea510
hyeonji817 <guswl930817@gmail.com>
2026-05-16 16:36:21 +0900

    orderList2.jsx - 이미지 갖고오기

--- client/src/shop/orderList2.jsx
+++ client/src/shop/orderList2.jsx
@@ -7,3 +7,5 @@ import Footer from "../main/Footer";
 import PaymentModal from "../components/PaymentModal";
 import beepBeep_Toy1 from "../assets/pet/1. beepBeep_Toy1.jpg";
 
+const CDN = (path) => `http://localhost:5003/uploads/${String(path || "").replace(/^\.\//,'')}`;
+
