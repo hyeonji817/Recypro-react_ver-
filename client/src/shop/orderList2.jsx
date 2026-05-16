b08ff2522ced60036cf611cffdbd45a5c96ad48e
hyeonji817 <guswl930817@gmail.com>
2026-05-16 21:59:04 +0900

    orderList2.jsx - 결제하기 구현

--- client/src/shop/orderList2.jsx
+++ client/src/shop/orderList2.jsx
@@ -436,7 +436,189 @@ const OrderList2 = () => {
                 </div>    {/** area_left end */}
               </div>    {/** ord_info end */}
 
-              
+              <div className="area_right">
+              <div className="inner">
+                  <div className="box">
+                    <h3 className="title first">결제 정보</h3>
+                    <table className="tbl_order2">
+                      <caption className="hidden">결제 가격정보</caption>
+                      <colgroup>
+                        <col style={{ width: "60%" }} />
+                        <col />
+                      </colgroup>
+
+                      <tbody>
+                        <tr>
+                          <th scope="row">상품합계 금액</th>
+                          <td>7,110 원</td>
+                        </tr>
+                        <tr className="total">
+                          <th scope="row">배송비 </th>
+                          <td>
+                            (+) <span id="delivery_prc2">3,000</span> 원
+                          </td>
+                        </tr>   {/** total end */}
+                      </tbody>
+                    </table>
+
+                    <table className="tbl_order2 sale">
+                      <caption className="hidden">결제 할인정보</caption>
+                      <colgroup>
+                        <col style={{ width: "50%" }} />
+                        <col />
+                      </colgroup>
+
+                      <tbody>
+                        <tr className="total order_area_total_sale_prc">
+                          <th scope="row">
+                            할인 금액 합계 <a className="i_info p_cursor"></a>
+                          </th>
+                          <td>
+                            (-) <span className="total_sale_prc">0</span> 원
+                            <div id="discount_info" className="view_info">
+                              <div className="order_area_event_prc" style={{ display: "none" }}>
+                                이벤트 할인금액<br />
+                                <span className="order_saleinfo_event_prc">0</span> 원
+                              </div>    {/** order_area_event_prc end */}
+                              <div className="order_area_timesale">
+                                타임세일금액<br />
+                                <span className="order_saleinfo_timesale">0</span> 원
+                              </div>    {/** order_area_timesale end */}
+                              <div className="order_area_member_prc" style={{ display: "none" }}>
+                                회원할인금액<br />
+                                <span className="order_saleinfo_member_prc">0</span> 원
+                              </div>    {/** order_area_member_prc end */}
+                              <div className="order_area_cpn_prc">
+                                쿠폰할인금액<br />
+                                <span className="order_saleinfo_cpn_prc">1,000</span> 원
+                              </div>    {/** order_area_cpn_prc end */}
+                              <div className="order_area_prd_prc" style={{ display: "none" }}>
+                                상품금액별할인금액<br />
+                                <span className="order_saleinfo_prd_prc">0</span> 원
+                              </div>    {/** order_area_prd_prc end */}
+                              <div className="order_area_prdcpn_prc" style={{ display: "none" }}>
+                                상품별쿠폰 할인금액<br />
+                                <span className="order_saleinfo_prdcpn_prc">0</span> 원
+                              </div>    {/** order_area_prdcpn_prc end */}
+                              <div className="order_area_sbscr_prc" style={{ display: "none" }}>
+                                정기배송 할인금액<br />
+                                <span className="order_saleinfo_sbscr_prc">0</span> 원
+                              </div>    {/** order_saleinfo_sbscr_prc end */}
+                            </div>    {/** view_info end */}
+                          </td>
+                        </tr>   {/** total order_area_total_sale_prc end */}
+
+                        <tr className="use_milage_field total_sale">
+                          <th scope="row">적립금 사용</th>
+                          <td>(-) <span className="use_milage_prc">0</span> 원</td>
+                        </tr>
+                        <tr className="use_emoney_field total_sale">
+                          <th scope="row">예치금 사용</th>
+                          <td>(-) <span className="use_emoney_prc">0</span> 원</td>
+                        </tr>   {/** use_emoney_field total_sale end */}
+                      </tbody>
+                    </table>
+
+                    <table className="tbl_order2">
+                      <caption className="hidden">결제정보</caption>
+                      <colgroup>
+                        <col style={{ width: "60%" }} />
+                        <col />
+                      </colgroup>
+
+                      <tbody>
+                        <tr className="total order_area_total_milage">
+                          <th scope="row">
+                            총 적립금 <a className="i_info p_cursor"></a>
+                          </th>
+                          <td>
+                            <span className="total_milage">158</span> 원
+                            <div id="milage_info" className="view_info">
+                              <div className="order_area_prd_milage">
+                                상품 적립금<br />
+                                <span className="order_saleinfo_prd_milage">0</span> 원
+                              </div>    {/** order_area_prd_milage end */}
+                              <div className="order_area_member_milage">
+                                회원 적립금<br />
+                                <span className="order_saleinfo_member_milage">158</span> 원
+                              </div>    {/** order_area_member_milage end */}
+                              <div className="order_area_event_milage">
+                                이벤트 적립금<br />
+                                <span className="order_saleinfo_event_milage">0</span> 원
+                              </div>    {/** order_area_event_milage end */}
+                            </div>    {/** view_info end */}
+                          </td>
+                        </tr>   {/** total order_area_total_milage end */}
+
+                        <tr>
+                          <th scope="row">총 결제 금액</th>
+                          <td>
+                            <strong className="total_price">
+                              <span className="order_info_sale_prc">10,110</span> 원
+                            </strong>   {/** total_price end */}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>    {/** tbl_order2 end */}
+
+                    <h3 className="title line">결제수단</h3>
+                    <div className="method">
+                      <div className="">
+                        <label className="pay_label">
+                          <input type="radio" name="pay_type" id="pay_type22" value="22" /> 
+                          <img src="https://x175-engine.mywisa.com/wm_engine_SW/_engine/card.tosspayment/image/tosspayment.png" height={18} />
+                        </label>    {/** pay_label end */}
+                      </div>
+                      <div className="">
+                        <label className="pay_label">
+                          <input type="radio" name="pay_type" id="pay_type17" value="17" /> 
+                          <span id="payco_btn_area">
+                            <img src="https://static-bill.nhnent.com//payco/checkout/img/v2/btn_type/EASYPAY_A1.png" width={103} height={14} style={{ width: "auto", verticalAlign: "middle" }} />
+                          </span>
+                        </label>	  {/** pay_label end */}
+                      </div>
+                      <p className="order_cancel_msg">
+                        * 주문신청 후 <strong className="point_color">3</strong>
+                        일 이내에 입금 확인이 되지 않으면 자동취소 됩니다.
+                      </p>    {/** order_cancel_msg end */}
+                      <div className="event_pay">
+                        <h3>결제프로모션</h3>
+                        <div className="field">
+                          <p className="th">Toss Pay</p>
+                          <p className="td">토스페이 결제 시 10% 추가할인<br />
+                            (결제 전 토스 앱 &gt; 토스페이 &gt; 쿠폰/혜택 받기 &gt; 결제 시 적용)
+                          </p>
+                        </div>    {/** field end */}
+                      </div>    {/** event_pay end */}
+        
+                      <div className="reconfirm">
+                        <label>
+                          <input name="reconfirm" id="reconfirm" type="checkbox" value="Y" /> 
+                          결제정보를 확인하였으며,<br />구매진행에 동의합니다.
+                        </label>
+                      </div>    {/** reconfirm end */}
+                      <div id="order1">
+                        <span className="box_btn huge block"><a href="/orderOk">주문하기</a></span>
+                      </div>    {/** order1 end */}
+                    </div>    {/** method end */}
+
+                    <div id="order2">
+                      <p className="total_info">
+                        총 결제 금액 
+                        <strong>
+                          <span className="order_info_sale_prc">29,000</span>
+                        </strong>
+                         원 결제를 합니다.
+                      </p>    {/** total_info end */}
+                      <p className="msg">&apos;결제하기&apos; 버튼을 누르면 결제창으로 연결됩니다.</p>
+                      <span className="box_btn w150 large">
+                        <input type="submit" value="결제하기" />
+                      </span>   {/** box_btn w150 large end */}
+                      <span className="box_btn w150 large white"><a href="#">취소</a></span>
+                    </div>    {/** order2 end */}
+                  </div>    {/** box end */}
+                </div>    {/** inner end */}
+              </div>      {/** area_right end */}
 
             </form>     {/** form end */}
           </div>      {/** order end */}  
