39e9ec949444dcaa975a6b9d11578063fb6fb145
hyeonji817 <guswl930817@gmail.com>
2026-06-07 18:29:58 +0900

    mypage_cancelRefund.jsx - 정보 수정하기 UI 구현

--- client/src/mypage/mypage_cancelRefund.jsx
+++ client/src/mypage/mypage_cancelRefund.jsx
@@ -10,7 +10,23 @@ const Mp_CancelRefund = () => {
         <Header_loginOK />  
       </div>      {/** mpCancel_Refund_Header end */}
 
-      <div className="mpCancel_Refund_Content"></div>     {/** mpCancel_Refund_Content end */}
+      <div className="mpCancel_Refund_Content">
+        <div className="mpCancel_Refund_body">
+          <h2 className="subtitle">MY PAGE</h2>     {/** subtitle end */}
+
+          <div className="mpCancel_Refund_top">
+            <div className="customer_section">
+              <div className="name"><strong>곽현지</strong>님은 MEMBER</div>      {/** name end */}
+
+              <a href="#" className="my_edit">정보 수정하기</a>    {/** my_edit end */}
+
+              <ul className="point_section"></ul>     {/** point_section end */} 
+
+              <ul className="mpCancel_Refund_tab"></ul>     {/** mpCancel_Refund_tab end */}
+            </div>    {/** customer_section end */}  
+          </div>     {/** mpCancel_Refund_top end */}
+        </div>      {/** mpCancel_Refund_body end */}  
+      </div>     {/** mpCancel_Refund_Content end */}
 
       <div className="mpCancel_Refund_Footer">
         <Footer />  
