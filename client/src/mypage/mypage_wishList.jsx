2da3cbf27f059cc64bcf29e1e3b5acbb42f101f3
hyeonji817 <guswl930817@gmail.com>
2026-06-13 23:33:40 +0900

    mypage_wishList.jsx - 페이지 내 상단 카테고리 구현

--- client/src/mypage/mypage_wishList.jsx
+++ client/src/mypage/mypage_wishList.jsx
@@ -20,13 +20,87 @@ const Mp_WishList = () => {
 
               <a href="#" className="my_edit">정보 수정하기</a>       {/** my_edit end */}
 
-              <ul className="point_section"></ul>       {/** point_section end */}
+              <ul className="point_section">
+                <li>
+                  <a href="#"><strong>쿠폰</strong> 0 장</a>
+                </li>
+                <li>
+                  <a href="#"><strong>적립금</strong> 0 원</a>
+                </li>
+                <li>
+                  <a href="#"><strong>포인트</strong> 0 P</a>  
+                </li>    
+              </ul>       {/** point_section end */}
 
-              <ul className="mpWishList_tab"></ul>      {/** mpWishList_tab end */} 
+              <ul className="mpWishList_tab">
+                <li>
+                  <a href="/orders" className="tab_title">
+                    ORDER
+                    <br />
+                    <strong className="tab_sub">주문내역</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/wishlist" className="tab_title">
+                    WISH LIST
+                    <br />
+                    <strong className="tab_sub">관심상품</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/coupons" className="tab_title">
+                    COUPON
+                    <br />
+                    <strong className="tab_sub">쿠폰</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/mileage" className="tab_title">
+                    MILEAGE
+                    <br />
+                    <strong className="tab_sub">적립금</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/deposits" className="tab_title">
+                    DEPOSITS
+                    <br />
+                    <strong className="tab_sub">예치금</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/special-mileage" className="tab_title">
+                    SECRET MILEAGE
+                    <br />
+                    <strong className="tab_sub">스페셜 적립금</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="tab_title">
+                    Q&amp;A
+                    <br />
+                    <strong className="tab_sub">내 상품문의</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="tab_title">
+                    MYBOARD
+                    <br />
+                    <strong className="tab_sub">내 상품평</strong>
+                  </a>
+                </li>
+                <li>
+                  <a href="/withdraw" className="tab_title">
+                    WITHDRAW
+                    <br />
+                    <strong className="tab_sub">회원 탈퇴</strong>
+                  </a>
+                </li>        
+              </ul>      {/** mpWishList_tab end */} 
             </div>     {/** customer_section end */}  
           </div>    {/**  mpWishList_top end */}  
 
-          
+
         </div>     {/** mpWishList_body end */}
       </div>    {/** mpWishList_Content end */}
 
