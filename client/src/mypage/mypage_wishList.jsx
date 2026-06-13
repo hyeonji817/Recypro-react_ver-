01e145d4620c5dfb8596ad1cbbc00894543c8f35
hyeonji817 <guswl930817@gmail.com>
2026-06-13 23:29:30 +0900

    mypage_wishList.jsx - 리턴문 내 페이지 화면 틀 잡기

--- client/src/mypage/mypage_wishList.jsx
+++ client/src/mypage/mypage_wishList.jsx
@@ -10,7 +10,25 @@ const Mp_WishList = () => {
         <Header_loginOK />  
       </div>     {/** mpWishList_Header end */}
 
-      <div className="mpWishList_Content"></div>    {/** mpWishList_Content end */}
+      <div className="mpWishList_Content">
+        <div className="mpWishList_body">
+          <h2 className="subtitle">MY PAGE</h2>     {/** subtitle end */}
+
+          <div className="mpWishList_top">
+            <div className="customer_section">
+              <div className="name"><strong>곽현지</strong>님은 MEMBER</div>      {/** name end */}
+
+              <a href="#" className="my_edit">정보 수정하기</a>       {/** my_edit end */}
+
+              <ul className="point_section"></ul>       {/** point_section end */}
+
+              <ul className="mpWishList_tab"></ul>      {/** mpWishList_tab end */} 
+            </div>     {/** customer_section end */}  
+          </div>    {/**  mpWishList_top end */}  
+
+          
+        </div>     {/** mpWishList_body end */}
+      </div>    {/** mpWishList_Content end */}
 
       <div className="mpWishList_Footer">
         <Footer />  
