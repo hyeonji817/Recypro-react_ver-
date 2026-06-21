ef8d5c3c7593c543177f53b5a7a0553fa4138f47
hyeonji817 <guswl930817@gmail.com>
2026-06-21 14:34:40 +0900

    mypage_coupon.jsx - 본문 내 카테고리 상단 1차 구현

--- client/src/mypage/mypage_coupon.jsx
+++ client/src/mypage/mypage_coupon.jsx
@@ -14,7 +14,19 @@ const Mp_Coupon = () => {
         <div className="mpCoupon_body">
           <h2 className="subtitle">MY PAGE</h2>       {/** subtitle end */}
 
-          <div className="mpCoupon_top"></div>      {/** mpCoupon_top end */}  
+          <div className="mpCoupon_top">
+            <div className="customer_section">
+              <div className="name"><strong>곽현지</strong>님은 MEMBER</div>      {/** name end */}  
+
+              <a href="#" className="my_edit">정보 수정하기</a>     {/** my_edit end */}
+
+              <ul className="point_section">
+                <li><a href="#"><strong>쿠폰</strong> 0 장</a></li>
+                <li><a href="#"><strong>적립금</strong> 0 원</a></li>
+                <li><a href="#"><strong>포인트</strong> 0 P</a></li>  
+              </ul>     {/** point_section end */}
+            </div>    {/** customer_section end */}
+          </div>      {/** mpCoupon_top end */}  
 
           <div className="coupon" id="coupon"></div>    {/** coupon end */}
         </div>       {/** mpCoupon_body end */}  
