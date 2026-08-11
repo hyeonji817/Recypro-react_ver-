1cbae8d508ea9b386fba6f99d1af7425a0897d35
hyeonji817 <guswl930817@gmail.com>
2026-08-11 21:54:04 +0900

    배포 직전 Step.4 - 서버용 .env 페이지와 클라이언트용 .env 페이지 분리

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import session from "express-session"; 
 import cors from "cors";    // 프론트엔드와 백엔드 간의 통신 허용. 
 import dotenv from "dotenv"; 
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
 // 라우트 파일 연결 
 import loginRouter from "./routes/login.js";
@@ -35,7 +35,7 @@ dotenv.config();
 const app = express();
 const port = 5003;    // 프론트엔드에서 호출할 포트와 일치해야 한다.   
 
-const sequelize = new Sequelize(
+/** const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -44,7 +44,7 @@ const sequelize = new Sequelize(
     port: Number(process.env.DB_PORT || 3306),
     dialect: "mysql",
   }
-);
+); */
 
 // CORS 설정 (프론트와 백 연결하는 징검다리)
 app.use(cors({
