2188ffc88c7ce30cdd9a885b04e77f60cc1e044e
hyeonji817 <guswl930817@gmail.com>
2026-08-19 15:37:12 +0900

    bcrypt 지우기

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,5 +1,4 @@
 import express from "express"; 
-import bcrypt from "bcrypt"; 
 import { db } from "../server.js"; 
 
 const router = express.Router(); 
@@ -8,7 +7,7 @@ router.post("/", (req, res) => {
   const { id, password } = req.body; 
   const sql = "SELECT * FROM user WHERE id = ?"; 
 
-  db.query(sql, [id], async (err, results) => {
+  db.query(sql, [id], (err, results) => {
     if (err) {
       console.error("[LOGIN] DB 오류:", err);
       return res.status(500).json({ message: "서버 오류" });
@@ -20,14 +19,12 @@ router.post("/", (req, res) => {
 
     const user = results[0];
 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    // 현재 DB 비밀번호가 평문이면 이 방식 사용
+    // 현재 DB 비밀번호가 평문인 경우
     if (user.password !== password) {
       return res.status(401).json({ message: "비밀번호 불일치" });
     }
 
-    req.session.userId = user.id;   // 사용자 세션 연결 
+    req.session.userId = user.id;
 
     console.log("[LOGIN] sessionID:", req.sessionID);
     console.log("[LOGIN] 저장 후 session:", req.session);
