5a374fb17d80f4f6378334779a636abb7f874cff
hyeonji817 <guswl930817@gmail.com>
2026-08-26 14:54:30 +0900

    픽셀단위 480 반응형 추가

--- client/src/main/Footer.jsx
+++ client/src/main/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
 						<span>사업자등록번호. 098-76-54321</span><br />
 						<span>주소. 서울특별시 서초구 리싸이프로대로 17길 08</span><br />
 						<span>통신판매업신고번호. 2010-서울강남-2010609호</span><br />
-						<span>개인정보보호책임자. 주영아</span>< br/>
+						<span>개인정보보호책임자. 주영아</span><br />
 						<span>대표자. 곽현지</span><br />
 						<span>E-MAIL. recypro@jww.co.kr</span><br />
 						<span>고객님은 안전거래를 위해 현금 등으로 결제 시 저희 쇼핑몰에서 가입한 PG 사의<br />
